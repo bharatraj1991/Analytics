de923fe63d5253bf38d1bcd834eeaad186036af3
bharatraj1991 <bharatraj.balani1991.com>
2018-09-30 09:42:46 +0530

    30th sep 9:30 am

--- data structure.R
+++ data structure.R
@@ -22,7 +22,8 @@ class(x3b)
 class(x4)
 
 x5=c(3L,5L)
-Class(x5)
+x5
+class(x5)
 x5a=c(3,5)
 class(x5a)
 (x5b=c(1,'a',T,4L))
@@ -40,11 +41,14 @@ x6[3] # access 3rd element
 x6[c(2,4)] # access 2nd and 4th element
 x6[-1] # access all but first element
 x6[-c(1:10)]
+x6[-c(1:10,15:20)]
+x6[c(2,-4)] # cannot mix positive and negative integers
 
 x6[c(2.4,3.54)]# real numbers are truncated to integers
 
 x6[-c(1,5,20)]
 x6[-length(x6)-1]
+length(x6)
 (x7=c(x6,x2))
 
 #modify
@@ -61,6 +65,8 @@ x[2]<-0;x # modify 2nd element
 x
 (x=-3:2)
 x<0
+x[x<=1 & x>=-1]=100;x
+x
 x[x<0]=5;x
 x[2]<- 10 :x
 
@@ -75,14 +81,19 @@ x[4]
 (x=rnorm(100)) # crteates 100 values with mean 0 and Sd 1
 ?rnorm
 plot(density(x))
-(x1=rnorm(1000,mean = 50,sd = 5)
-
+mean(x)
+(x1 = rnorm(1000000, mean=50, sd=5))
+plot(density(x1))
+abline(v=mean(x1),h=0.04)
+?abline
 # Matrices----
+100:111
   (m1=matrix(1:12,nrow = 4))
   (m2=matrix(1:12,ncol = 3,byrow = T))
 x= 101:124
 length(x)
 matrix(x,ncol =6 )
+class(m1)
 attributes(m1)
 dim(m1)
 m1
@@ -97,6 +108,35 @@ paste("C",1:100,sep="-")
 (colnames(m1)= paste('C',1:3,sep ='' ))
 m1
 (rownames(m1)= paste('R',1:4,sep ='' ))
+m1
+attributes(m1)
+dim(m1)
+
+#vector to matrix
+(m3=1:24)
+dim(m3)=c(6,4)
+m3
+
+# access elements
+m2
+m2[1,]
+m2[c(1,3,4),]
+m2[,1]
+m2[,2:3]
+m2
+m2[c(1,2),c(2,3)]
+m2[,]
+m2[-2,]
+m2[1:5]
+m2
+m2[c(TRUE,F,T,F),c(F, T, T)] #logical indexing
+m2[m2 > 5 & m2 < 10]
+
+m1
+m1[1:2,1:2]
+m1[c('R1'),c('C1','C3')]
+m1[1:2,]
+m1[c(T,T,F,F),]
 m1
 
 sweep(m1,MARGIN = 1,STATS = c(2,3,4,5),FUN = "+")
